1aec11568c32f719ef5b3e858e8fd7ef901c50da
SquirrelloDev <olejnike2000@gmail.com>
2023-03-27 16:52:25 +0200

    Changed the signs from text "icons" to svg icons

--- src/components/Vote/Vote.js
+++ src/components/Vote/Vote.js
@@ -1,11 +1,12 @@
 import classes from "../../sass/components/Vote.module.scss";
-
+import plusIcon from "../../images/icon-plus.svg";
+import minusIcon from "../../images/icon-minus.svg";
 const Vote = () => {
   return (
       <div className={classes.vote}>
-          <button className={classes.vote__btn}>+</button>
+          <button className={classes.vote__btn}><img src={plusIcon} alt={"Upvote icon"}/></button>
           <div className={classes.vote__score}><span>12</span></div>
-          <button className={classes.vote__btn}>-</button>
+          <button className={classes.vote__btn}><img src={minusIcon} alt={"Downvote icon"}/></button>
       </div>
   )
 }
