c95c9053a2ff697c3e83e64e773da8f183524f2d
SquirrelloDev <olejnike2000@gmail.com>
2023-03-28 18:00:17 +0200

    Implemented populating initial data

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,11 +1,22 @@
 import Comment from "./Comment";
 import useFileData from "../../hooks/use-file-data";
+import {useEffect} from "react";
 
 const Comments = () => {
     const {fileData} = useFileData('./data.json');
     const populateInitData = () => {
+        if(!localStorage.getItem('currentUser') || !localStorage.getItem('comments')){
+            localStorage.setItem('currentUser', JSON.stringify(fileData.currentUser))
+            localStorage.setItem('comments', JSON.stringify(fileData.comments))
+        }
+        else{
+            console.warn("Data already pupulated")
+        }
 
     }
+    useEffect(() =>{
+        populateInitData();
+    }, [populateInitData])
     return (
         <div>
             <Comment replies={[1]}/>
