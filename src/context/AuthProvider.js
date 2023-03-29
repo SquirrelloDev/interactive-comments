60dd559e698233a2f0e8e2f40ac26f84ff9d3613
SquirrelloDev <olejnike2000@gmail.com>
2023-03-29 16:09:44 +0200

    Wrapped populateUser function inside useCallback hook

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,13 +1,13 @@
 import authContext, {initialUser} from "./auth-context";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import useFileData from "../hooks/use-file-data";
 const AuthProvider = ({children}) => {
     const {getFileData} = useFileData()
     const [user, setUser] = useState(initialUser);
-    const populateUser = async () => {
+    const populateUser = useCallback(async () => {
         const data = await getFileData('./data.json');
         setUser(data.currentUser);
-    }
+    }, [getFileData]) 
     useEffect(() => {
         populateUser();
     }, [populateUser])
