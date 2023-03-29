6eb6657ced4be9ca400e2bc0a86514812d5a766a
SquirrelloDev <olejnike2000@gmail.com>
2023-03-29 15:39:37 +0200

    Added comment ownership recognition

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -6,22 +6,30 @@ import replyIcon from '../../images/icon-reply.svg'
 import editIcon from '../../images/icon-edit.svg'
 import deleteIcon from '../../images/icon-delete.svg'
 import ReplyContainer from "../Replies/ReplyContainer";
+import {useContext} from "react";
+import authContext from "../../context/auth-context";
 const Comment = ({id,content, createdAt, user, replies, activeComment, setActiveComment}) => {
+    const authCtx = useContext(authContext);
     // const setReply = () =>{
     //     setReplyComment(id);
     // }
 
     const {username, image} = user;
-    const isUserComment = username;
+    const isUserComment = username === authCtx.username;
   return (
       <>
           <div className={classes.comment}>
-              <div className={classes.comment__header}><Avatar srcImg={image.png}/><span>{username}</span><span>{createdAt}</span></div>
+              <div className={classes.comment__header}><Avatar srcImg={image.png}/><span>{username}</span> {isUserComment && <span className={classes.comment__header__badge}>you</span>} <span>{createdAt}</span> </div>
               <div className={classes.comment__content}><p>{content}</p></div>
               <div className={classes.comment__vote}><Vote/></div>
               <div className={classes.comment__actions}>
-                  {/*<IconButton onClickFn={() => setActiveComment({id, type:'editing'})} icon={editIcon}>Edit</IconButton>*/}
-                  <IconButton onClickFn={() => setActiveComment({id, type:'replying'})} icon={replyIcon}>Reply</IconButton>
+                  {isUserComment &&
+                      <>
+                          <IconButton danger icon={deleteIcon}>Delete</IconButton>
+                          <IconButton onClickFn={() => setActiveComment({id, type:'editing'})} icon={editIcon}>Edit</IconButton>
+                      </>
+                      }
+                  {!isUserComment && <IconButton onClickFn={() => setActiveComment({id, type:'replying'})} icon={replyIcon}>Reply</IconButton> }
               </div>
           </div>
           {replies.length > 0 && <ReplyContainer setReplyComment={setActiveComment} replies={replies}/>}
